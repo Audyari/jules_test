f2491efd0290a9ec9bce72bf4c75087bf75c5b1b
Audyari <audy123ari@gmail.com>
2026-05-08 09:44:45 +0700

    ini data berpindah jadi gk bisa di akses.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
     let x = 5;
-    println!("Nilai x adalah: {}", x);
-    x = 6;
-    println!("Nilai x sekarang adalah: {}", x);
+    println!("Nilai x adalah: {x}");
+    //x = 6; // yah gk bisa lah kan x bersifat berpindah
+    println!("Nilai x sekarang adalah: {x}");
 }
